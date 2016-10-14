c0ec11c0e1ce89d833ccb362bc5775a8b67955c0
Zacck <coderv63@gmail.com>
2016-10-14 19:55:02 +0200

    added higher order functionality to component two and some cool overrides

--- app/components/ComponentTwo.jsx
+++ app/components/ComponentTwo.jsx
@@ -1,5 +1,51 @@
 import React from 'react'
 
+//higher order component functionality
+// var isAdmin = true;
+// var adminComponent = (Component) => {
+//   return class Admin extends  React.Component {
+//     render() {
+//       if (isAdmin) {
+//         return(
+//           <div className="callout secondary">
+//             <p className="alert label">Private Admin Information</p>
+//             <Component {...this.props}/>
+//           </div>
+//         );
+//       } else
+//       {
+//           return null;
+//       }
+//     }
+//   };
+// };
+
+var isAdmin = true;
+var adminComponent = (Component) => {
+  return class Admin extends  Component {
+    componentDidUpdate () {
+      console.log('Admin Component did update');
+      if(super.componentDidUpdate) {
+        super.componentDidUpdate();
+      }
+    }
+    render() {
+      if (isAdmin) {
+        return(
+          <div className="callout secondary">
+            <p className="alert label">Private Admin Information</p>
+            {super.render()}
+          </div>
+        );
+      } else
+      {
+          return null;
+      }
+    }
+  };
+};
+
+
 //make a class
 class ComponentTwo extends React.Component {
   //override constructor
@@ -10,6 +56,9 @@ class ComponentTwo extends React.Component {
     }
     //bind the handler to the component
     this.onClick = this.onClick.bind(this)
+  }
+  componentDidUpdate() {
+    console.log('component 2 did update');
   }
   onClick () { //bind on clice
     this.setState({
@@ -26,16 +75,17 @@ class ComponentTwo extends React.Component {
       </div>
     )
   }
-}
+};
 
 
 //setting default props
 ComponentTwo.defaultProps = {
   count: 50
-}
+};
 
 //setting prop types
 ComponentTwo.propTypes = {
   count: React.PropTypes.number
-}
-export default ComponentTwo;
+};
+
+export default adminComponent(ComponentTwo);
